5398b37db7d55908b453fd3698afa1f06e22e378
noehassiel <hassiel-monterrosas@hotmail.com>
2022-11-16 21:10:33 -0600

    cambios en links

--- js/jquerys.js
+++ js/jquerys.js
@@ -1,11 +1,13 @@
 /*Footer*/
-$("#fBlockOne").hover(function () {
+$("#fBlockOne").hover(
+  function () {
     // over
     $("#fBlockOne .footer-block-bg").css({
       height: "100%",
       transition: "all .4s ease",
     });
-  }, function () {
+  },
+  function () {
     // out
     $("#fBlockOne .footer-block-bg").css({
       height: "0",
@@ -14,13 +16,15 @@ $("#fBlockOne").hover(function () {
   }
 );
 
-$("#fBlockTwo").hover(function () {
+$("#fBlockTwo").hover(
+  function () {
     // over
     $("#fBlockTwo .footer-block-bg").css({
       height: "100%",
       transition: "all .4s ease",
     });
-  }, function () {
+  },
+  function () {
     // out
     $("#fBlockTwo .footer-block-bg").css({
       height: "0",
@@ -29,13 +33,15 @@ $("#fBlockTwo").hover(function () {
   }
 );
 
-$("#fBlockThree").hover(function () {
+$("#fBlockThree").hover(
+  function () {
     // over
     $("#fBlockThree .footer-block-bg").css({
       height: "100%",
       transition: "all .4s ease",
     });
-  }, function () {
+  },
+  function () {
     // out
     $("#fBlockThree .footer-block-bg").css({
       height: "0",
@@ -44,40 +50,44 @@ $("#fBlockThree").hover(function () {
   }
 );
 
-
-$("#fBlockOne a").hover(function () {
+$("#fBlockOne a").hover(
+  function () {
     // over
-    $("#fBlockOne .s-f-block-title").css('color', 'var(--green-system)')
-  }, function () {
+    $("#fBlockOne .s-f-block-title").css("color", "var(--green-system)");
+  },
+  function () {
     // out
-    $("#fBlockOne .s-f-block-title").css('color', 'var(--white)')
+    $("#fBlockOne .s-f-block-title").css("color", "var(--white)");
   }
 );
 
-$("#fBlockTwo a").hover(function () {
+$("#fBlockTwo a").hover(
+  function () {
     // over
-    $("#fBlockTwo .s-f-block-title").css('color', 'var(--green-system)')
-  }, function () {
+    $("#fBlockTwo .s-f-block-title").css("color", "var(--green-system)");
+  },
+  function () {
     // out
-    $("#fBlockTwo .s-f-block-title").css('color', 'var(--white)')
+    $("#fBlockTwo .s-f-block-title").css("color", "var(--white)");
   }
 );
 
-$("#fBlockThree a").hover(function () {
+$("#fBlockThree a").hover(
+  function () {
     // over
-    $("#fBlockThree .s-f-block-title").css('color', 'var(--green-system)')
-  }, function () {
+    $("#fBlockThree .s-f-block-title").css("color", "var(--green-system)");
+  },
+  function () {
     // out
-    $("#fBlockThree .s-f-block-title").css('color', 'var(--white)')
+    $("#fBlockThree .s-f-block-title").css("color", "var(--white)");
   }
 );
 
 $(document).ready(function () {
-
   $(".c-cards .main").hide();
   $(".c-cards .others").hide();
   /*Product cards*/
-  $(".option").click(function(){
+  $(".option").click(function () {
     $(".option").removeClass("active");
     $(this).addClass("active");
   });
@@ -105,21 +115,20 @@ $(document).ready(function () {
 
     $(".c-cards .others").fadeIn();
   });
-
 });
 
-$('#navMovilB').click(function (e) {
+$("#navMovilB").click(function (e) {
   e.preventDefault();
 
-  $('.nav-movil').addClass('show');
+  $(".nav-movil").addClass("show");
 
-  $('body').css('overflow', 'hidden');
+  $("body").css("overflow", "hidden");
 });
 
-$('#closeB').click(function (e) {
+$("#closeB").click(function (e) {
   e.preventDefault();
 
-  $('.nav-movil').removeClass('show');
+  $(".nav-movil").removeClass("show");
 
-  $('body').css('overflow', 'inherit');
+  $("body").css("overflow", "inherit");
 });
