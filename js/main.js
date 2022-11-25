2aff199d1b411cf685d4c893b170885d4d8035a0
Noé Hassiel Monterrosas Bandala <hassiel-monterrosas@hotmail.com>
2022-11-25 15:46:08 -0600

    soluciones

--- js/main.js
+++ js/main.js
@@ -200,7 +200,7 @@ var swiper = new Swiper(".prodSwiper", {
 var animation = bodymovin.loadAnimation({
   // animationData: { /* ... */ },
   container: document.getElementById('icon-container'), // required
-  path: 'data.json', // required
+  path: '../data.json', // required
   renderer: 'svg', // required
   loop: true, // optional
   autoplay: true, // optional
